6f1ac5a4508ee3112e6f37a77251d31f15ddda17
medoookkk <48764289+medoookkk@users.noreply.github.com>
2020-05-06 22:56:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -868,7 +868,7 @@ client.on('message', PuP => {
 
 client.on('ready', function(){
     var ms = 10000 ;
-    var setGame = [' -help',`Im on ${client.guilds.size} servers`,`i help ${client.users.size} Members`,' BAD Is The Best ','Just Do It'];
+    var setGame = [' -help',' MEDOOO_96 Is The Best ','MEDOOO_96'];
     var i = -1;
     var j = 0;
     setInterval(function (){
