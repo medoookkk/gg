f191c3696fc2f653b4606e625f061990665505a4
medoookkk <48764289+medoookkk@users.noreply.github.com>
2020-05-12 16:12:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,6 +52,15 @@ if(message.content.startsWith(prefix + "Owner")) {
 });
 
 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+});
+
+
 
 
   
